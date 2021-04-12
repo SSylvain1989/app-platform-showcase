2a0548adae1334b96cf56fe8436e83903c24286f
Chris Sev <chris@better.dev>
2021-04-12 14:02:34 -0700

    footer is done

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -1,3 +1,13 @@
 export default function SiteFooter() {
-  return <div>site footer</div>;
+  return (
+    <div className="bg-blue-900 py-3 text-blue-300 font-semibold text-sm text-center">
+      Hosted on{" "}
+      <a
+        className="text-blue-100 underline hover:text-white"
+        href="https://www.digitalocean.com/products/app-platform/"
+      >
+        DigitalOcean App Platform
+      </a>
+    </div>
+  );
 }
